24ca4b541b099284c31902c481d9059837f52c43
Ar273404 <arun273404@gmail.com>
2024-06-08 14:08:56 +0530

    main

---
+++ ludo.js
@@ -0,0 +1,8 @@
+var step = 49.5;
+var current_color = "";
+var NumOfPaw = "";
+var num = 0;
+var clicked = false;
+var current_pawn = "";
+var allcolor = ["red", "blue", "green", "yellow"];
+var pawnOut = {red:0,blue:0,green:0,yellow:0}
